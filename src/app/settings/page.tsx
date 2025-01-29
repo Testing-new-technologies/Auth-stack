a648681e8cc3480c6e510889ebf635b6cd99317e
Lostovayne <moises.barrera.dev@gmail.com>
2025-01-28 21:18:53 -0300

    first commit

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@stackframe/stack"
+import { ReactElement } from 'react'
+
+
+const Page = ({}): ReactElement => {
+  return (
+    <div className="container mx-auto" >
+      <UserButton/>
+    </div>
+  );
+};
+
+export default Page;
